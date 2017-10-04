689934568939301fccd912eaf013266f9497098e
darren <764190362@qq.com>
2017-10-04 21:34:38 +0800

    基本框架搭建完成

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTagHelpersExamples.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreTagHelpersExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(repository.Cites);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City city)
+        {
+            repository.AddCity(city);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTagHelpersExamples.Models
+{
+    public interface IRepository
+    {
+        IEnumerable<City> Cites { get; }
+        void AddCity(City newCity);
+    }
+}
